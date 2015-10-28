34ea57b4d3bf62daa823c88f6f57ddd017c77254
Kameron R. Sween <kameron@krsween.com>
2015-10-28 17:39:26 -0400

    delinted

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -15,54 +15,53 @@ export class HomeCmp {
     this.testSuites = [
       {
         id: 1,
-        suiteName: "Suite 1",
+        suiteName: 'Suite 1',
         builds: this.mockBuilds()
       },
       {
         id: 2,
-        suiteName: "Suite 2",
+        suiteName: 'Suite 2',
         builds: this.mockBuilds()
       },
       {
         id: 3,
-        suiteName: "Suite 3",
+        suiteName: 'Suite 3',
         builds: this.mockBuilds()
       },
       {
         id: 4,
-        suiteName: "Suite 4",
+        suiteName: 'Suite 4',
         builds: this.mockBuilds()
       },
       {
         id: 5,
-        suiteName: "Suite 5",
+        suiteName: 'Suite 5',
         builds: this.mockBuilds()
       },
       {
         id: 6,
-        suiteName: "Suite 6",
+        suiteName: 'Suite 6',
         builds: this.mockBuilds()
       },
       {
         id: 7,
-        suiteName: "Suite 7",
+        suiteName: 'Suite 7',
         builds: this.mockBuilds()
       },
       {
         id: 8,
-        suiteName: "Suite 8",
+        suiteName: 'Suite 8',
         builds: this.mockBuilds()
       }
     ];
-    this.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    this.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   }
 
 
   formatTime(timestamp: string) {
     var date = new Date(timestamp),
         day = date.getDate(),
-        monthIndex = date.getMonth(),
-        year = date.getFullYear();
+        monthIndex = date.getMonth();
 
 
 
@@ -78,16 +77,16 @@ export class HomeCmp {
           mockBuild = {
             id: i,
             date: mockDate,
-            status: "pass",
+            status: 'pass',
             totalSpecs: 0,
             totalPasses: Math.round(Math.random()*10),
             totalFails: Math.round(Math.random()*10),
             totalSkips: Math.round(Math.random()*10),
-            url: "http://www.jenkins.com"
+            url: 'http://www.jenkins.com'
           };
           mockBuild.totalSpecs = mockBuild.totalPasses + mockBuild.totalFails + mockBuild.totalSkips;
-          if(mockBuild.totalFails > 0){
-            mockBuild.status = "fail";
+          if (mockBuild.totalFails > 0) {
+            mockBuild.status = 'fail';
           }
           mockBuildData.push(mockBuild);
     }
