e6ca403a8c0f4d54c4faec909a510e91b0d2d49c
Kameron R. Sween <kameron@krsween.com>
2015-10-30 09:52:27 -0400

    refactored this morning's work. everything needed to be initialized before I could call += on it. #stupid

---
+++ app/components/home/column.ts
@@ -0,0 +1,75 @@
+export class SingleColumnTotal {
+  date: string;
+
+  totalPasses: number = 0;
+  totalFails: number = 0;
+  buildFails: number = 0;
+  totalSpecs: number = 0;
+  runs: number = 0;
+  totalSkips: number = 0;
+
+  constructor (date: string, totals: any) {
+    this.date = date;
+    this.updateTotal(totals);
+  }
+
+  updateTotal (newTotals: any) {
+    this.totalSpecs = newTotals.totalSpecs || (newTotals.totalPasses + newTotals.totalFails);
+    this.totalPasses += newTotals.totalPasses || 0;
+    this.totalFails += newTotals.totalFails || 0;
+    this.buildFails += newTotals.status === 'fail' ? this.buildFails += 1 : null;
+    this.totalSkips += newTotals.totalSkips || 0;
+    this.runs += 1;
+  }
+
+  getName () : string {
+    return this.date;
+  }
+}
+
+
+export class ColumnContainer {
+  columns: Array<SingleColumnTotal> = [];
+
+  getColumnIndex(key: string): number {
+    let indexOfKey: number = -1;
+
+    this.columns.forEach((column, index) => {
+      if(column.getName() === key) {
+        indexOfKey = index;
+      }
+    });
+
+    return indexOfKey;
+  }
+
+  addTotals(date: string,  totals: any): void {
+    var columnIndex = this.getColumnIndex(date),
+        newTotals = {
+          totalSpecs: totals.totalSpecs || (totals.totalPasses + totals.totalFails),
+          totalPasses: totals.totalPasses || 0,
+          totalFails: totals.totalFails || 0,
+          status: totals.status || '',
+          totalSkips: totals.totalSkips || 0
+        };
+
+    if(columnIndex === -1) {
+      var column = new SingleColumnTotal(date, newTotals);
+      this.columns.push(column);
+    } else {
+      this.columns[columnIndex].updateTotal(newTotals);
+    }
+  }
+
+  deleteColumn(key:string): void {
+    this.columns.forEach((column, index) => {
+      if(column.getName() === key) {
+        this.columns.splice(index, 1);
+      }
+    });
+  }
+
+  getColumns(): Array<SingleColumnTotal> {
+    return this.columns;
+  }
+}
--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -3,6 +3,8 @@ import { Component, NgFor, NgIf, NgClass } from 'angular2/angular2';
 
 import { MockSuites } from '../../services/mock_suites';
 
+import { ColumnContainer, SingleColumnTotal } from './column';
+
 
 
 @Component({
@@ -42,10 +44,10 @@ export class HomeCmp {
 
         var totalObj = {
           totalSpecs: build.totalSpecs || 0,
-          specPasses: build.totalPasses || 0,
-          specFails: build.totalFails || 0,
+          totalPasses: build.totalPasses || 0,
+          totalFails: build.totalFails || 0,
           status: build.status || 0,
-          skips: build.totalSkips || 0,
+          totalSkips: build.totalSkips || 0,
         };
 
         columnTotals.addTotals(build.date.toString(), totalObj);
@@ -54,119 +56,5 @@ export class HomeCmp {
     this.columnTotals = columnTotals.getColumns();
 
     console.log(this.columnTotals);
-  }
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class SingleColumnTotal {
-  date: string;
-  specPasses: number;
-  specFails: number;
-  buildFails: number;
-  totalSpecs: number;
-  runs: number;
-  skips: number;
-
-  constructor (date: string, totals: any) {
-    this.date = date;
-    this.specPasses = totals.specPasses || 0;
-    this.specFails = totals.specFails || 0;
-    this.buildFails = totals.status === 'fail' ? 1 : 0;
-    this.runs = totals.runs || 0;
-    this.skips = totals.skips || 0;
-    this.totalSpecs = totals.totalSpecs || (totals.specPasses + totals.specFails);
-  }
-
-  updateTotal (newTotals: any) {
-    this.totalSpecs = newTotals.totalSpecs || (newTotals.specPasses + newTotals.specFails);
-    this.specPasses += newTotals.specPasses || 0;
-    this.specFails += newTotals.specFails || 0;
-    this.buildFails += newTotals.status === 'fail' ? this.buildFails += 1 : null;
-    this.skips += newTotals.skips || 0;
-    this.runs += 1;
-  }
-
-  getName () : string {
-    return this.date;
-  }
-}
-
-
-class ColumnContainer {
-  columns: Array<SingleColumnTotal> = [];
-  constructor () {}
-
-  getColumnIndex(key: string): number {
-    let indexOfKey: number = -1;
-
-    this.columns.forEach((column, index) => {
-      if(column.getName() === key) {
-        indexOfKey = index;
-      }
-    });
-
-    return indexOfKey;
-  }
-
-  addTotals(date: string,  totals: any): void {
-    var columnIndex = this.getColumnIndex(date),
-
-      newTotals = {
-        totalSpecs: totals.totalSpecs || (totals.specPasses + totals.specFails),
-        specPasses: totals.specPasses || 0,
-        specFails: totals.specFails || 0,
-        status: totals.status || '',
-        skips: totals.skips || 0
-      };
-
-    if(columnIndex === -1) {
-      var column = new SingleColumnTotal(date, newTotals);
-      this.columns.push(column);
-    } else {
-      this.columns[columnIndex].updateTotal(newTotals);
-    }
-  }
-
-  deleteColumn(key:string): void {
-    this.columns.forEach((column, index) => {
-      if(column.getName() === key) {
-        this.columns.splice(index, 1);
-      }
-    });
-  }
-
-  getColumns(): Array<SingleColumnTotal> {
-    return this.columns;
   }
 }
